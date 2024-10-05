cfd349fb18a39bd602e6e64e0ba0e29eb37a546b
Bruno Perez <konobabuno@gmail.com>
2024-10-04 23:46:45 -0600

    top

--- js/top.js
+++ js/top.js
@@ -3,12 +3,32 @@ $(function(){
 })
 
 function consultar(){
+
+    // Obtener la fecha actual
+    const fechaActual = new Date();
+    const año = fechaActual.getFullYear();
+    const mes = ('0' + (fechaActual.getMonth() + 1)).slice(-2);
+    const dia = ('0' + fechaActual.getDate()).slice(-2);
+
+    // Formatear fecha en 'YYYY-MM-DD'
+    const fechaFormateada = `${año}-${mes}-${dia}`;
+
     $.ajax({
-        url: 'https://api.mediterrum.site/reporte/top-usuarios/2023-01-01/6',
+        url: `https://api.mediterrum.site/reporte/top-usuarios/${fechaFormateada}/10`,
         method: 'GET',
         contentType: 'application/json',
         success: function(response) {
-            console.log(response);
+             // Iterar sobre la respuesta para agregar los usuarios
+        response.forEach((usuario, index) => {
+            $('#top-filas').append(`
+                <div class="col-12 col-md-6">
+                    <p>${index + 1}. ${usuario.nombre}</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <p class="text-md-center">Puntaje Mensual: ${usuario.total_puntos}</p>
+                </div>
+            `);
+        });
         },
         error: function(xhr, status, error) {
             console.error('Error:', error);
