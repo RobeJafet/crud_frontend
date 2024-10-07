7abd7aef68e0c96a656203fa5e075601cc65f112
Bruno Perez <konobabuno@gmail.com>
2024-10-07 08:29:51 -0600

    pass

---
+++ js/password.js
@@ -0,0 +1,36 @@
+
+const params = new URLSearchParams(window.location.search);
+const token = params.get('token');
+const userId = params.get('id');
+
+$(function() {
+    $('#password-form').submit(function(e) {
+        e.preventDefault();
+        var password = $('#password-input').val();
+
+        const JSONData = JSON.stringify({
+            nuevaContrasena: password
+        });
+
+        $.ajax({
+            url: `http://localhost:3011/${userId}/contrasena`, // Asegúrate de que esta URL coincide con la ruta que has definido
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSONData,
+            headers: {
+                'Authorization': `Bearer ${token}` // Añadir el token en el encabezado
+            },
+            success: function(response) {
+                window.location.href = './usuarios.html';
+            },
+            error: function(xhr, status, error) {
+                console.log("ERROR");
+            }
+        });
+    });
+
+    // Redirigir si el token no está presente
+    if (!token) {
+        window.location.href = './login.html';
+    }
+});
